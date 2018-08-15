cb6a4e3be91bca91595158c93bf209fdc5b83767
Gregor Cox <gregorcox@gmail.com>
2018-08-15 11:23:21 +0100

    bucketlist.js methods created

---
+++ client/src/models/bucket_list.js
@@ -0,0 +1,42 @@
+const PubSub = require('../helpers/pub_sub.js');
+const Request = require('../helpers/request.js');
+
+const BucketList = function(url){
+  this.url = url;
+}
+
+BucketList.prototype.getData = function () {
+  const request = new Request(this.url);
+  request.get()
+  .then((items) => {
+    PubSub.publish('Items:data-loaded', items);
+  })
+  .catch(console.error);
+};
+
+BucketList.prototype.bindEvents = function () {
+  PubSub.subscribe('BucketView:item-delete-clicked', (evt) =>{
+    this.deleteItem(evt.detail);
+  })
+  PubSub.subscribe('BucketView:item-submitted', (evt) =>{
+    this.postItem(evt.detail);
+  })
+};
+
+BucketList.prototype.postItem = function (item) {
+  const request = new Request(this.url);
+  request.post(item)
+  .then((items) => {
+    PubSub.publish('Items:data-loaded', items);
+  })
+  .catch(console.error);
+};
+
+BucketList.prototype.deleteItem = function (itemId) {
+  const request = new Request(this.url);
+  request.delete(itemId)
+  .then((items) =>{
+    PubSub.publish('Items:data-loaded', items);
+  })
+  .catch(console.error);;
+};
