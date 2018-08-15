c364625bfe80dc59b6bff2677991b73b7691ba3e
Gregor Cox <gregorcox@gmail.com>
2018-08-15 11:07:33 +0100

    create route created and tested in insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -18,14 +18,36 @@ const createRouter = function (collection) {
 
   //SHOW
   router.get('/:id', (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     collection.findOne({ _id: ObjectId(id) })
-    .then((doc) => {
-      req.json(doc)
+      .then((doc) => {
+        res.json(doc);
+      })
+  });
+
+  //DESTROY
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection.deleteOne({ _id: ObjectId(id) })
+      .then(() => {
+        collection.find().toArray()
+          .then((docs) => res.json(docs));
+      })
+  })
+
+  //CREATE
+  router.post('/', (req,res) => {
+    const newItem = req.body
+    collection.insertOne(newItem)
+    .then(() => {
+      collection.find().toArray()
+      .then((docs) =>{
+        res.json(docs)
+      })
     })
     .catch((err) => {
       console.error(err);
-    });
+    })
   });
 
 return router;
